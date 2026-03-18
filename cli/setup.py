cf98af4dc0bb298c36c927a16f6c4b096e680063
windbug99 <windbug99@gmail.com>
2026-03-18 14:48:11 +0900

    chore: bump project version to 0.1.4

--- cli/setup.py
+++ cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="openrssgate",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "typer==0.16.1",
